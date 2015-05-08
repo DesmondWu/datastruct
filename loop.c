5ffe924e10554b0cf511f79de225c95ac723a608
Desmond Wu <wkunhui@gmail.com>
2015-05-08 12:11:27 +0800

    Update loop.c

--- loop.c
+++ loop.c
@@ -1,4 +1,16 @@
-
+/*
+; Copyright (C) 2015 Desmond Wu <wkunhui@gmail.com>
+;
+; This software is licensed under the terms of the GNU General Public
+; License version 2, as published by the Free Software Foundation, and
+; may be copied, distributed, and modified under those terms.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; reference doc:http://blog.ostermiller.org/find-loop-singly-linked-list
+*/
 #include "stdio.h"
 #include "stdlib.h"
 typedef struct
@@ -45,6 +57,11 @@ void linkcat(link *pselect_link)
 {
 	link *cur;
 	int i;
+	if(checkloop(pselect_link)!=0)
+	{
+		printf("loop detected!!\n");
+		return;
+	}
 	for(cur=pselect_link,i=0;cur!=NULL;cur=cur->next,i++)
 	{				
 		printf("idx[%d]=%d \n",i,cur->data);	
@@ -66,8 +83,8 @@ void trap(link *pselect_link,int count)
 		}
 	}
 }
-//select_link loop checker hash with pRecord_link
-int hash(link *pSelect_link,link *pRecord_link)
+//select_link loop checker hash 
+int hash(link *pSelect_link)
 {
 /*	sample code
   HashSet nodesSeen = new HashSet();
@@ -79,38 +96,50 @@ int hash(link *pSelect_link,link *pRecord_link)
   return false;
 */  
 }
-//select_link loop checker nlogn(btree) with pRecord_link
-int btree(link *pSelect_link,link *pRecord_link)
+//select_link loop checker nlogn(tree)
+int tree(link *pSelect_link)
 {
-	
+/*
+sample code
+http://web.mit.edu/~emin/www.old/source_code/red_black_tree/red_black_tree.c	
+*/
 }
-//select_link loop checker n^2(doubly) with pRecord_link
-int doubly(link *pSelect_link,link *pRecord_link)
+//select_link loop checker n^2(doubly) 
+int doubly(link *pSelect_link)
 {
-	
+	link *pcur,*prec;
+	link record={0,NULL};
+	if(pSelect_link==NULL) return -1;//no link data
+	if(pSelect_link->next==NULL) return 0; //no loop
+	record.data=pSelect_link;
+	for(pcur=pSelect_link->next;pcur!=NULL;pcur=pcur->next)
+	{	
+		for(prec=&record;prec!=NULL;prec=prec->next)
+		{
+			if(prec->data==pcur)
+			{	
+				return -1;
+			}
+		}
+		add(&record,pcur);
+	}
+	return 0;	
 }
 int checkloop(link *pSelect_link)
 {
-	link *plnew,*pcur;
-	link record={0,NULL};
 	if(pSelect_link==NULL) return -1;//no link data
 
 	if(pSelect_link->next==NULL) return 0; //no loop
-	record.data=pSelect_link;
-	for(pcur=pSelect_link->next;pcur->next!=NULL;pcur=pcur->next)
-	{
-		add(&record,pcur);
-		doubly(pcur,&record);
-	}
+	if(doubly(pSelect_link)) return -1;
+
+	return 0;
 		
 }
 int main()
 {
 	link head={0,NULL};
-	add(&head,1);
-	add(&head,2);
-	add(&head,3);
-	add(&head,4);
+	int i;
+	for(i=1;i<10;i++) add(&head,i);
 	linkcat(&head);
 	trap(&head,2);
 	linkcat(&head);
